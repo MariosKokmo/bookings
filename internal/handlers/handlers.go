cee4015c15b559c91a8916f9477d59ac5ab8c2f3
MariosKokmo <pjpastakijuniormarios@yahoo.gr>
2024-02-06 21:52:07 +0000

    Updates

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -134,8 +134,34 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+
+	// route to reservation summary
+	// first we store the data into the session
+	m.App.Session.Put(r.Context(), "reservation", reservation)
+
+	// redirect to summary page
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
 
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		log.Println("cannot get item from session")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	// at this point we have taken the reservation, don't need it anymore
+	m.App.Session.Remove(r.Context(), "reservation")
+	
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
